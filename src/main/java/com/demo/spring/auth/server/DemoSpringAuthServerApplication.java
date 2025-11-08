ce286dae865a3e58ee8f278a5e16b2d4eabd6281
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-11-08 10:15:09 +0530

    Oauth2.0 Authserver setup

---
+++ src/main/java/com/demo/spring/auth/server/DemoSpringAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring.auth.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringAuthServerApplication.class, args);
+    }
+}
